7c6af431e6bd084d9b27405c422af5f9aa29cae5
Somplesi <40087258+Somplesi@users.noreply.github.com>
2020-06-15 12:23:42 +0200

    Ajoutez des fichiers projet.

---
+++ magicnumber/Views/GamePage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace magicnumber.views
+{
+    public partial class GamePage : ContentPage
+    {
+        const int NB_MIN = 1;
+        const int NB_MAX = 10;
+        int magicNumber = 0;
+
+        public GamePage()
+        {
+            InitializeComponent();
+            magicNumber = new Random().Next(NB_MIN, NB_MAX);
+            minMaxLabel.Text = "Entre " + NB_MIN + " et " + NB_MAX;
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        public GamePage(Entry entryNumber)
+        {
+            this.entryNumber = entryNumber;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            int nombreUtilisateur = 0;
+
+            try
+            {
+                nombreUtilisateur = Int32.Parse(entryNumber.Text);
+            }
+            catch (Exception)
+            {
+                DisplayAlert("Oops", "Vous devez saisie un nombre entier", "OK");
+                entryNumber.Text = "";
+                return;
+            }
+
+            if (((nombreUtilisateur < NB_MIN) || (nombreUtilisateur > NB_MAX)) && (String.IsNullOrWhiteSpace(entryNumber.Text))) {
+                DisplayAlert("Attention", "Vous devez rentrer un nombre entre " + NB_MIN + " et" + NB_MAX, "OK");
+                entryNumber.Text = "";
+                return;
+            }
+
+            if (magicNumber > nombreUtilisateur)
+            {
+                DisplayAlert("Perdu", "Le nombre magique est supérieur à " + nombreUtilisateur, "OK");
+                return;
+            }
+            if (magicNumber < nombreUtilisateur)
+            {
+                DisplayAlert("Perdu",  "Le nombre magique est inférieur à " + nombreUtilisateur, "OK");
+                return;
+            }
+            if (magicNumber == nombreUtilisateur)
+            {
+#pragma warning disable CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+                WinAction();
+#pragma warning restore CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+                return;
+            }
+        }
+
+
+        private async Task WinAction()
+        {
+            await DisplayAlert("Gagné", "Vous avez trouvé le nombre magique", "OK");
+            await this.Navigation.PopAsync();
+        }
+
+    }
+}
---
+++ magicnumber/WelcomePage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using magicnumber.views;
+using Xamarin.Forms;
+
+namespace magicnumber
+{
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+#pragma warning disable CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+            InfiniteRotation(star1, 5000);
+            InfiniteRotation(star1, 7000);
+            InfiniteRotation(star1, 9000);
+            InfiniteScale(playButton, 1000);
+#pragma warning restore CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+        }
+
+        private async Task InfiniteRotation(View view, uint length)
+        {
+            bool always = true;
+            while (always)
+            {
+                await star1.RotateTo(360, length);
+                star1.Rotation = 0;
+            }
+        }
+
+        private async Task InfiniteScale(View view, uint length)
+        {
+            bool always = true;
+            while (always)
+            {
+                await view.ScaleTo(1.1, length);
+                await view.ScaleTo(1.0, length);
+            }
+        }
+
+        void PlayButton(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new GamePage());
+        }
+    }
+}
